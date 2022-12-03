110636a9d04f75bd7132d5b3d3455945e8c742c7
Георгий Ляпунов <lyapunovgeorgy@yandex.ru>
2022-12-03 08:09:19 +0500

    Добавьте файлы проекта.

---
+++ UchebnayaPraktika/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UchebnayaPraktika
+{
+    public partial class FormMenu : Form
+    {
+        FormWork1 Work1 = new FormWork1();
+        public FormMenu()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Work1.ShowDialog();
+        }
+    }
+}
---
+++ UchebnayaPraktika/Form2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UchebnayaPraktika
+{
+    public partial class FormWork1 : Form
+    {
+        public FormWork1()
+        {
+            InitializeComponent();
+        }
+    }
+}
