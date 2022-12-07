5f88a0ed0fa8a87fa7f63bf7448bdbf7997cf772
el_patriot <lyapunovgeorgy@yandex.ru>
2022-12-07 16:45:02 +0500

    Задание четвёртое. Проблемы с выводом картинки пользователя в picturebox1.

--- UchebnayaPraktika/Form5.Designer.cs
+++ UchebnayaPraktika/Form5.Designer.cs
@@ -28,20 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 185);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
-            this.dataGridView1.TabIndex = 0;
             // 
             // pictureBox1
             // 
@@ -50,26 +47,83 @@
             this.pictureBox1.Size = new System.Drawing.Size(776, 167);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column5,
+            this.Column6,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 185);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ФИО";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Логин";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Уровень доступа";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Доступные комнаты";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "URL-ссылка на фото";
+            this.Column4.Name = "Column4";
             // 
             // FormWork4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 348);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "FormWork4";
             this.Text = "Form5";
             this.Load += new System.EventHandler(this.Form5_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     }
 }
--- UchebnayaPraktika/Form5.cs
+++ UchebnayaPraktika/Form5.cs
@@ -7,17 +7,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
 
 namespace UchebnayaPraktika
 {
     public partial class FormWork4 : Form
     {
+        MySqlConnection conn;
+        int rows;
+        public class con
+        {
+            static public MySqlConnection Connection()
+            {
+                string con = $"server=chuc.caseum.ru;port=33333;user=st_1_20_20;database=is_1_20_st20_KURS;password=65218328;";
+                MySqlConnection conn = new MySqlConnection(con);
+                return conn;
+            }
+        }
         public FormWork4()
         {
             InitializeComponent();
+            conn = con.Connection();
+            try
+            {
+                conn.Open();
+                string sql = $"SELECT\r\n\tauth_data.id_auth, \r\n\tauth_data.fio_auth, \r\n\tauth_data.login, \r\n\tauth_data.accessed_rooms_auth, \r\n\tauth_data.level_access_auth, \r\n\tauth_data.photo_auth\r\nFROM\r\n\tauth_data\r\n\tINNER JOIN\r\n\tpersonnel\r\n\tON \r\n\t\tauth_data.fio_auth = personnel.id_personnel";
+                //наследуемая таблица / поле наследующей таблицы / поле наследуемой таблицы                                                          
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    rows = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rows].Cells[0].Value = reader[0].ToString();
+                    dataGridView1.Rows[rows].Cells[1].Value = reader[1].ToString();
+                    dataGridView1.Rows[rows].Cells[2].Value = reader[2].ToString();
+                    dataGridView1.Rows[rows].Cells[3].Value = reader[3].ToString();
+                    dataGridView1.Rows[rows].Cells[4].Value = reader[4].ToString();
+                    dataGridView1.Rows[rows].Cells[5].Value = reader[5].ToString();
+                    
+
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
         }
 
         private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var url = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            pictureBox1.ImageLocation = url;
+            MessageBox.Show("Выведено");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
